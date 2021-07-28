d41da82f864424ab688885ae5fbe23259ae2e84f
v0xat <v0xt.alex@gmail.com>
2021-07-28 23:10:31 +0300

    Edit home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
 const HomePage = () => (
-  <div>
-    <h3 className="text-center mt-5 mb-4">Добро пожаловать на главную страницу!</h3>
+  <div className="d-flex flex-column align-items-center">
+    <h1 className="mt-5 mb-4">Привет!</h1>
+    <p style={{ fontSize: 19 }}>
+      Ты находишься на главной странице приложения-чата, сделанного в рамках
+      <a href="https://ru.hexlet.io/programs/frontend/projects/12"> проекта </a>
+      на Hexlet.
+      <br />
+      Проект не завершён, поэтому для входа в чат используйте данные пользователя
+      <mark> admin </mark> (username: <mark>admin</mark>, password: <mark>admin</mark>)
+      <br />
+      Какие технологии я использовал:
+    </p>
+    <ul style={{ fontSize: 19 }}>
+      <li>React, Redux-toolkit</li>
+      <li>Socket.io</li>
+      <li>Bootstrap</li>
+      <li>Webpack</li>
+      <li>Formik</li>
+    </ul>
   </div>
 );
 
